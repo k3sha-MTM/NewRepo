14e24e93b942b88f33d8e8514b440d09319b0823
student <student@05-07>
2021-09-08 11:36:30 +0300

    Client answer

--- Client/Client/Program.cs
+++ Client/Client/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace Client
 {
     class Program
     {
+        static string ipAddr = "127.0.0.1";
+        static int port = 8000;
+        static int a = 2;
+        static int b = 2;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse(ipAddr), port);
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                socket.Connect(iPEndPoint);
+                int bytes = 0;
+                byte[] data = new byte[256];
+                StringBuilder stringBuilder = new StringBuilder();
+                do
+                {
+                    bytes = socket.Receive(data);
+                    stringBuilder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                } while (socket.Available > 0);
+                Console.WriteLine(stringBuilder.ToString());
+
+                Console.WriteLine("Enter quastion for server...");
+                string sms = Console.ReadLine();
+                data = Encoding.Unicode.GetBytes(sms);
+                socket.Send(data);
+                Console.WriteLine($"Sms \"{a+b}\" send to SERVER [{ipAddr}]!");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
